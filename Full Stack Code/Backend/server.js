74791759b0a874a3d977d8b97a4eb75bf43759b4
Bhavesh143-py <pratikshakotkar98@gmail.com>
2024-11-25 18:21:44 +0530

    added port for backend

--- Full Stack Code/Backend/server.js
+++ Full Stack Code/Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/index'); // Import central routes index
-
+require('dotenv').config();
 app.use(express.json());
 app.use(require('cors')()); // Enable CORS if needed
 app.use('/api', routes); // Use the central router for all API routes
